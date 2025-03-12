7ee329039280f0f549f42d2fb66468e58ae1f0a2
Leslie Williamson <leslie.williamson@live.ca>
2025-03-12 17:52:04 -0400

    Add Chatroom and Sessions controller tests. Resolved issue with Chatroom controller by changing from plural to singular. What a PITA to figure that one out.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the login path" do
+    get login_url
+    assert_response :success
+  end
 end
