4b97dc863f32713c8489e4d53f75007e6c35e6d4
Leslie Williamson <leslie.williamson@live.ca>
2025-03-18 14:29:57 -0400

    Add semantic-ui

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "semantic-min", to: "https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.js"
+pin "google-ajax", to: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"
