c4d79f5b8a85128f6ad161ffd9922c95633e056f
Leslie Williamson <leslie.williamson@live.ca>
2025-03-25 11:28:22 -0400

    Add Session and Chatroom controller tests

--- test/controllers/chatroom_controller_test.rb
+++ test/controllers/chatroom_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class ChatroomControllerTest < ActionDispatch::IntegrationTest
-  test "the root path" do
+  test "the root path forbidden when not logged in" do
     get root_url
-    assert_response :success
+    assert_response :redirect
+    follow_redirect!
+    assert_equal login_path, path
+  end
+
+  test "the root path accessible when logged in" do
+    sign_in_as(users(:user1))
+    assert_response :redirect
+    follow_redirect!
+    assert_equal root_path, path
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,4 +5,23 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_url
     assert_response :success
   end
+
+  test "loggin in" do
+    sign_in_as(users(:user1))
+    assert_response :redirect
+    follow_redirect!
+    assert_equal root_path, path
+  end
+
+  test "logging out" do
+    sign_in_as(users(:user1))
+    assert_response :redirect
+    follow_redirect!
+    assert_equal root_path, path
+
+    sign_out
+    assert_response :redirect
+    follow_redirect!
+    assert_equal login_path, path
+  end
 end
