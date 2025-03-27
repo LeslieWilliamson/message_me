912260b6e838cd86aa627b2262a5f3ff8485d8a4
Leslie Williamson <leslie.williamson@live.ca>
2025-03-26 21:19:42 -0400

    Hide button and send chat message when enter key is pressed.
    Issue: input field is not cleared after message is sent.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,11 +9,26 @@ window.scroll_bottom = function () {
     }
 }
 
+window.submit_message = function () {
+    $('message_body').on('keydown', function (event) {
+        if (event.keyCode == 13) {
+            $('button').click();
+            event.target.value = ''; // this didn't work
+            // $('message_body').val(''); // this didn't work either
+        }
+    })
+}
+
+// $(document).bind("ajax:beforeSend", function () {
+//     $('#message_body').val('');
+// });
+
 $(document).on('turbo:load', function () {
     $(".ui.dropdown").dropdown();
 
     $('.message .close').on('click', function () {
         $(this).closest('.message').transition('fade');
     });
+    submit_message();
     scroll_bottom();
 });
